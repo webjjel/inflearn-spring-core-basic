cda5901b49b54b3411175d3300bcd4cc931d8999
DESKTOP-JGG5GOE\김갑훈 <webjjel@saramin.co.kr>
2022-04-15 15:42:00 +0900

    의존관계 자동 주입
    * 옵션 처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -13,8 +13,4 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
 }
